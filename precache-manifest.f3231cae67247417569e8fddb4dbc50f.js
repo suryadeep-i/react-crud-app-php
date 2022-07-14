0eb0ef4b33dc1ee34c42947b7c77cfdd5a84a19d
suryadeep-i <suryadeep.ivaturi@gmail.com>
2022-07-15 02:11:08 +0530

    Updates

---
+++ precache-manifest.f3231cae67247417569e8fddb4dbc50f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7452e8551fb9ac75cd8",
+    "url": "/react_php/static/css/main.d3859cba.chunk.css"
+  },
+  {
+    "revision": "d7452e8551fb9ac75cd8",
+    "url": "/react_php/static/js/main.d7452e85.chunk.js"
+  },
+  {
+    "revision": "6bb1b744dd079d1587a5",
+    "url": "/react_php/static/js/1.6bb1b744.chunk.js"
+  },
+  {
+    "revision": "f8d2db5d953417513189",
+    "url": "/react_php/static/js/runtime~main.f8d2db5d.js"
+  },
+  {
+    "revision": "a15d4a27778739c699814da21d0de770",
+    "url": "/react_php/index.html"
+  }
+];
